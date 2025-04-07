890f937ea2b322bda840a20c4a9ca00c53575045
achahid19 <anasks1999@gmail.com>
2025-04-07 15:21:33 +0100

    Implemeting formatted dates

--- beta-app/script.js
+++ beta-app/script.js
@@ -7,19 +7,45 @@
 // Data hard-coded for learning puposes
 // in real-life, we use data mostly from APIs
 const account1 = {
-  owner: 'Jonas Smith',
-  movements: [200, 450.489, -400.32, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
-
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
-
+	owner: 'Jonas Schmedtmann',
+	movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+	interestRate: 1.2, // %
+	pin: 1111,
+  
+	movementsDates: [
+	  '2019-11-18T21:31:17.178Z',
+	  '2019-12-23T07:42:02.383Z',
+	  '2020-01-28T09:15:04.904Z',
+	  '2020-04-01T10:17:24.185Z',
+	  '2020-05-08T14:11:59.604Z',
+	  '2020-05-27T17:01:17.194Z',
+	  '2020-07-11T23:36:17.929Z',
+	  '2020-07-12T10:51:36.790Z',
+	],
+	currency: 'EUR',
+	locale: 'pt-PT', // de-DE
+  };
+  
+  const account2 = {
+	owner: 'Jessica Davis',
+	movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+	interestRate: 1.5,
+	pin: 2222,
+  
+	movementsDates: [
+	  '2019-11-01T13:15:33.035Z',
+	  '2019-11-30T09:48:16.867Z',
+	  '2019-12-25T06:04:23.907Z',
+	  '2020-01-25T14:18:46.235Z',
+	  '2020-02-05T16:33:06.386Z',
+	  '2020-04-10T14:43:26.374Z',
+	  '2020-06-25T18:49:59.371Z',
+	  '2020-07-26T12:01:20.894Z',
+	],
+	currency: 'USD',
+	locale: 'en-US',
+  };
+  
 const account3 = {
   owner: 'Steven Thomas Williams',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
@@ -34,7 +60,7 @@ const account4 = {
   pin: 4444,
 };
 
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -64,14 +90,21 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 
 // built a display movements function
-const displayMovs = function(movements) {
+const displayMovs = function(movements, movementsDates) {
 	containerMovements.innerHTML = ''; // clear container.
-	movements.forEach(function(movement, i){
+	
+	movements.forEach(function(movement, i) {
+		const movDate = new Date(`${movementsDates[i]}`);
+		const day = `${movDate.getDate()}`.padStart(2, 0);
+		const month = `${movDate.getMonth() + 1}`.padStart(2, 0); // Month in js in 0 based lol.
+		const year = `${movDate.getFullYear()}`;
+		const getDate = `${day}/${month}/${year}`;
 		const type = movement > 0 ? 'deposit' : 'withdrawal';
 		const htmlEl = `<div class="movements__row">
-          <div class="movements__type movements__type--${type}">${i} ${type}</div>
-          <div class="movements__value">${movement.toFixed(2)}€</div>
-        </div>`;
+			<div class="movements__type movements__type--${type}">${i} ${type}</div>
+			<div class="movements__date">${getDate}</div>
+			<div class="movements__value">${movement.toFixed(2)}€</div>
+			</div>`;
 
 		containerMovements.insertAdjacentHTML('afterbegin', htmlEl);
 	})
@@ -118,9 +151,18 @@ const calcDisplaySummary = function(acc) {
 // but the call must be after definition.
 const updateUI = () => {
 	displayBalance(loginUser);
-	displayMovs(loginUser['movements']);
+	displayMovs(loginUser['movements'], loginUser['movementsDates']);
 	calcDisplaySummary(loginUser);
 }
+
+// FAKE the login. /////////////
+
+/*
+let	loginUser = accounts[0];
+updateUI();
+containerApp.style.opacity = 1; */
+
+///////////////////////////////
 
 let	loginUser;
 
@@ -157,7 +199,9 @@ btnTransfer.addEventListener('click', function(e) {
 		&& recipient?.userName != loginUser.userName
 		) {
 			loginUser.movements.push(-transferValue);
+			loginUser['movementsDates'].push(new Date().toISOString());
 			recipient.movements.push(transferValue);
+			recipient['movementsDates'].push(new Date().toISOString());
 			
 			// auto update UI for logged user.
 			updateUI();
@@ -197,7 +241,8 @@ btnLoan.addEventListener('click', function(e) {
 		amount > 0
 		&& loginUser.movements.some(mov => mov >= amount * 0.10)
 	) {
-		loginUser.movements.push(amount);
+		loginUser['movements'].push(amount);
+		loginUser['movementsDates'].push(new Date().toISOString());
 		updateUI();
 	}
 	inputLoanAmount.value = '';
@@ -209,7 +254,7 @@ let sorted = false;
 btnSort.addEventListener('click', function(e) {
 	e.preventDefault();
 
-	sorted ? displayMovs(loginUser['movements'])
+	sorted ? displayMovs(loginUser['movements'], loginUser['movementsDates'])
 			: displayMovs(
 				loginUser['movements']
 				.slice()
@@ -217,9 +262,13 @@ btnSort.addEventListener('click', function(e) {
 				// a - b > 0. no swap
 				// a - b < 0. swap
 				// a - b == 0. nothing done
+				, loginUser['movementsDates'] 
+				// dates needs to be sorted again, for simplicity im going to keep them intact.
 			);
 	sorted = !sorted;
 })
+
+// P.S: there is no error management system for this program.
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
